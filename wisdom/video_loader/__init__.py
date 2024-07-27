14382b5e0e998d7136b31fbb92467a84d34d54a3
akhildevelops <akhilgodvsdemon@gmail.com>
2024-07-28 04:46:40 +0530

    Downloads Video from external url

--- wisdom/video_loader/__init__.py
+++ wisdom/video_loader/__init__.py
@@ -3,7 +3,8 @@ from typing import Iterator,Optional
 from dataclasses import dataclass
 from datetime import timedelta
 import numpy
-
+import requests
+from pathlib import Path
 @dataclass
 class VideoMetadata:
     n_frames:int
@@ -13,7 +14,7 @@ class VideoMetadata:
 
     @property
     def frame_time(self)->float:
-        return timedelta(seconds=1/self.fps)
+        return 1/self.fps
     
     @property
     def video_length(self)->timedelta:
@@ -37,5 +38,19 @@ class VideoLoader:
                 return
             yield cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
             counter+=1
+    @classmethod
+    def from_url(cls,url:str,path:Optional[str]=None,restrict_frames:Optional[int]=None):
+        if not path:
+            path="./retail_store.mp4"
+        print("Downloading video from %s to %s "%(url,path))
+        if Path(path).exists():
+            print("Skipping download as %s is already present"%path)
+        loader = requests.get(url)
+        with open(path, 'wb') as f:
+                for chunk in loader.iter_content(chunk_size=1024 * 4): 
+                    f.write(chunk)
+        print("Finished Downloading")
+        return cls(path,restrict_frames)
+
 
 
